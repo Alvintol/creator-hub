4d79c809dfd3c07b0cce9aa34113fecc15ae8d03
Alvin <inbox@alvintolentino.com>
2026-04-10 22:42:50 -0600

    created auth/redirect pages and Creator application page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import ScrollToTop from './components/ScrollToTop';
-import ProfileSetupRedirect from './components/ProfileSetupRedirect';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import ScrollToTop from "./components/ScrollToTop";
+import ProfileSetupRedirect from "./components/ProfileSetupRedirect";
+import RequireAuth from "./components/RequireAuth";
 
-import Home from './pages/Home';
-import Live from './pages/Live';
-import Creators from './pages/Creators';
-import CreatorProfile from './pages/CreatorProfile';
-import Market from './pages/Market';
-import Listing from './pages/Listing';
-import NotFound from './pages/NotFound';
+import Home from "./pages/Home";
+import Live from "./pages/Live";
+import Creators from "./pages/Creators";
+import CreatorProfile from "./pages/CreatorProfile";
+import Market from "./pages/Market";
+import Listing from "./pages/Listing";
+import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import ProfileSettings from "./pages/ProfileSettings";
-
+import ApplyCreator from "./pages/ApplyCreator";
 
 const App = () => {
   return (
     <>
       <ScrollToTop />
       <ProfileSetupRedirect />
+
       <Routes>
         <Route element={<Layout />}>
-          <Route path='/' element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/live' element={<Live />} />
-          <Route path='/market' element={<Market />} />
-          <Route path='/creators' element={<Creators />} />
-          <Route path='/listing/:id' element={<Listing />} />
-          <Route path='/creator/:handle' element={<CreatorProfile />} />
-          <Route path="/settings/profile" element={<ProfileSettings />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/live" element={<Live />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/creators" element={<Creators />} />
+          <Route path="/listing/:id" element={<Listing />} />
+          <Route path="/creator/:handle" element={<CreatorProfile />} />
+
+          <Route element={<RequireAuth />}>
+            <Route path="/settings/profile" element={<ProfileSettings />} />
+            <Route path="/apply/creator" element={<ApplyCreator />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
